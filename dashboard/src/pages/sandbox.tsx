34971eb09692bb2aa0d30b3b9bc9b4858ca672d8
dre <reh@boldreh.com>
2024-09-24 07:20:07 +0200

    ollama: make weather tool call actual geocoding and weather api

--- dashboard/src/pages/sandbox.tsx
+++ dashboard/src/pages/sandbox.tsx
@@ -18,6 +18,26 @@ import {
 import { memo, useCallback, useState } from "react";
 import Markdown from "react-markdown";
 
+// See: https://nominatim.org/release-docs/develop/api/Output/
+// there are more fields...
+interface NominatimGeocodingResult {
+  lat: string;
+  lon: string;
+  name: string;
+  display_name: string;
+}
+
+export async function nominatimGeocode(q: string) {
+  const res: NominatimGeocodingResult[] = await (
+    await fetch(
+      `https://nominatim.openstreetmap.org/search.php?q=${encodeURIComponent(
+        q
+      )}&format=jsonv2`
+    )
+  ).json();
+  return res;
+}
+
 // Make modifications to the theme with your own fields and widgets
 
 const Form = withTheme(SemanticUITheme);
@@ -207,16 +227,73 @@ const weatherTool: CallableTool = {
   },
   async fn(arg: { location: string }) {
     const location = String(arg.location);
-    if (location === "Berlin") {
-      return JSON.stringify({
+    const weatherCodeToHumanReadable: { [k: string]: string } = {
+      "0": "clear sky",
+      "1": "mainly clear",
+      "2": "partly cloudy",
+      "3": "overcast",
+      "45": "fog",
+      "48": "depositing rime fog",
+      "51": "light drizzle",
+      "53": "moderate drizzle",
+      "55": "dense intensity drizzle",
+      "56": "freezing drizzle",
+      "57": "freezing drizzle with dense intensity",
+      "61": "slight rain",
+      "63": "moderate rain",
+      "65": "heavy rain",
+      "66": "light freezing rain",
+      "67": "heavy freezing rain",
+      "71": "slight snow fall",
+      "73": "heavy snow fall",
+      "75": "heavy snow fall",
+      "77": "snow grains",
+      "80": "slight rain showers",
+      "81": "moderate rain showers",
+      "82": "violent rain showers",
+      "85": "slight snow showers",
+      "86": "heavy snow showers",
+      "95": "thunderstorm",
+      "96": "thunderstorm with slight hail",
+      "99": "thunderstorm with heavy hail",
+    };
+
+    try {
+      const geocodedPlace = (await nominatimGeocode(location))[0];
+      const weather = await (
+        await fetch(
+          `https://api.open-meteo.com/v1/forecast?latitude=${encodeURIComponent(
+            geocodedPlace.lat
+          )}&longitude=${encodeURIComponent(
+            geocodedPlace.lon
+          )}&current=temperature_2m,weather_code&hourly=temperature_2m,weather_code&forecast_days=1`
+        )
+      ).json();
+      //console.log(weather);
+      const toolResult = {
         name: "get_weather",
         location,
-        celsius: 24,
-      });
-    } else {
+        current: `${weather.current.temperature_2m}${
+          weather.current_units.temperature_2m
+        }, ${
+          weatherCodeToHumanReadable[`${weather.current.weather_code}`] || ""
+        }`,
+        forecast: weather.hourly.temperature_2m.map(
+          (t: number, i: any) =>
+            `${weather.hourly.time[i]}: ${t}${
+              weather.hourly_units.temperature_2m
+            }, ${
+              weatherCodeToHumanReadable[`${weather.hourly.weather_code[i]}`] ||
+              ""
+            }`
+        ),
+      };
+      //console.log("toolresult:", toolResult);
+      return JSON.stringify(toolResult);
+    } catch (e) {
       return JSON.stringify({
         name: "get_weather",
-        error: `no data for ${location}`,
+        error: `unable to retrieve data for ${location}`,
       });
     }
   },
